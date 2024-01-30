c05b2e52447a01447b99c0f3035b316ee2ff5c26
msseock <msseock@gmail.com>
2024-01-30 18:24:35 +0900

    Build: Xcode 프로젝트 파일 생성
    최소 빌드 버전 iOS 16.0으로 설정

---
+++ ReadingFrame/ReadingFrame/ReadingFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingFrameApp.swift
+//  ReadingFrame
+//
+//  Created by 석민솔 on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadingFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

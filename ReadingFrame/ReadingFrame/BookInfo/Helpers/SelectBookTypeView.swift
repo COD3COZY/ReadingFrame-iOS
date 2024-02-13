50573dcb9934591c920da57b2777192eeae85b49
msseock <msseock@gmail.com>
2024-02-13 19:48:19 +0900

    Feat: Book Model 생성 #6
    모델 생성, 책등록을 위한 뷰 파일 만들기만 해둠

---
+++ ReadingFrame/ReadingFrame/BookInfo/Helpers/SelectBookTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectBookTypeView.swift
+//  ReadingFrame
+//
+//  Created by 석민솔 on 2/13/24.
+//
+
+import SwiftUI
+
+struct SelectBookTypeView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    SelectBookTypeView()
+}

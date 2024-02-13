bc6ca149d9ccca4ae168e0b00d0c4123a5a98dbc
Yoonji Lee <yooonji08@naver.com>
2024-02-13 18:52:41 +0900

    Feat: 홈 화면 및 책장 이동 버튼 UI 구현 #4

---
+++ ReadingFrame/ReadingFrame/Common/Helpers/HomeSegmentedControl.swift
@@ -0,0 +1,38 @@
+//
+//  HomeSegmentedControl.swift
+//  ReadingFrame
+//
+//  Created by 이윤지 on 2/13/24.
+//
+
+import SwiftUI
+
+/// 홈 화면 및 책장 화면에서 사용되는 Segmented Control
+struct HomeSegmentedControl: View {
+    
+    /// Default 메뉴 값 (홈 화면)
+    @State var selection: String = "book.closed"
+    
+    /// Segmented Control에 들어갈 메뉴 값 (홈 화면, 책장 화면)
+    let filterOptions: [String] = [
+        "book.closed", "books.vertical"
+    ]
+    
+    var body: some View {
+        Picker(selection: $selection,
+               label: Text("Picker"),
+               content: {
+                ForEach(filterOptions.indices) { index in
+                    Image(systemName: filterOptions[index])
+                        .font(.system(size: 13).weight(.semibold))
+                        .foregroundStyle(.black)
+                        .tag(filterOptions[index])
+            }
+        })
+        .pickerStyle(SegmentedPickerStyle())
+    }
+}
+
+#Preview {
+    HomeSegmentedControl()
+}

ed4727519d134773333048b2d27d92d459b26923
Yoonji Lee <yooonji08@naver.com>
2024-02-13 18:52:00 +0900

    Feat: 검색 바 UI 구현 #4
    - 홈 화면 및 상세 책 조회 화면에서 사용되는 검색 바 UI 구현 완료

---
+++ ReadingFrame/ReadingFrame/Common/Helpers/SearchBar.swift
@@ -0,0 +1,49 @@
+//
+//  SearchBar.swift
+//  ReadingFrame
+//
+//  Created by 이윤지 on 2/13/24.
+//
+
+import SwiftUI
+
+/// 홈 화면 및 책 상세 화면에서 사용되는 검색 바
+struct SearchBar: View {
+    
+    /// 사용자가 입력한 검색어
+    @State var searchText: String = ""
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                
+                TextField("제목, 작가를 입력하세요", text: $searchText)
+                
+                // 검색어가 있다면
+                if (!searchText.isEmpty) {
+                    // 취소 버튼 클릭 시, 입력된 검색어 지우기
+                    Button(action: {
+                        self.searchText = ""
+                    }) {
+                        // 취소 버튼 띄우기
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                }
+                // 검색어가 없다면
+                else {
+                    // TODO: 홈 화면으로 이동?
+                    
+                }
+            }
+            .padding(EdgeInsets(top: 8, leading: 7, bottom: 8, trailing: 7))
+            .foregroundStyle(.greyText)
+            .background(Color(.grey1))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+#Preview {
+    SearchBar()
+}

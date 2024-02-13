d7429747498d2f36ebb1f7c35ea21b6b4d576c45
Yoonji Lee <yooonji08@naver.com>
2024-02-13 23:35:09 +0900

    Feat: 홈 화면에 책이 등록되지 않았을 때의 UI 구현 #4

--- ReadingFrame/ReadingFrame/MainPage/Views/MainPage.swift
+++ ReadingFrame/ReadingFrame/MainPage/Views/MainPage.swift
@@ -7,12 +7,140 @@
 
 import SwiftUI
 
+/// 홈 화면
 struct MainPage: View {
+    @State private var wantToReadBooksCount = 0 /// 읽고 싶은 책 갯수
+    @State private var readingBooksCount = 0 /// 읽고 있는 책 갯수
+    @State private var finishReadBooksCount = 0 /// 다 읽은 책 갯수
+    
     var body: some View {
-        Text("This is Main Page!")
+        ScrollView() {
+            VStack(alignment: .leading) {
+                VStack(alignment: .trailing) {
+                    // 검색바
+                    SearchBar()
+                    
+                    // 홈 화면, 책장 화면 전환 버튼
+                    HomeSegmentedControl()
+                        .frame(width: 118, height: 28)
+                        .padding(.top, 15)
+                }
+                .padding(.top, 10)
+                
+                // 읽고 있는 책
+                HStack {
+                    Text("읽고 있는 책")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                    
+                    Text("\(readingBooksCount)")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                }
+                
+                // 등록된 책 갯수에 따른 뷰 처리
+                // 등록된 책이 있다면
+                if (readingBooksCount >= 1) {
+                    
+                }
+                // 등록된 책이 없다면
+                else {
+                    notRegisteredBook()
+                }
+                
+                // 읽고 싶은 책
+                HStack {
+                    Text("읽고 싶은 책")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                    
+                    Text("\(wantToReadBooksCount)")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                }
+                .padding(.top, 47)
+                
+                // 등록된 책 갯수에 따른 뷰 처리
+                // 등록된 책이 있다면
+                if (wantToReadBooksCount >= 1) {
+                    
+                }
+                // 등록된 책이 없다면
+                else {
+                    notRegisteredBook()
+                }
+                
+                // 다 읽은 책
+                HStack {
+                    Text("다 읽은 책")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                    
+                    Text("\(finishReadBooksCount)")
+                        .font(.thirdTitle)
+                        .foregroundStyle(.black0)
+                }
+                .padding(.top, 47)
+                
+                // 등록된 책 갯수에 따른 뷰 처리
+                // 등록된 책이 있다면
+                if (finishReadBooksCount >= 1) {
+                    
+                }
+                // 등록된 책이 없다면
+                else {
+                    notRegisteredBook()
+                }
+                
+            }
+            .padding(.leading, 16)
+            .padding(.trailing, 16)
+            
+            Spacer()
+        }
+        // 화면 전체 스크롤 가능하도록 설정
+        .frame(maxWidth: .infinity)
+    }
+}
+
+/// 책이 등록되지 않았을 때의 뷰
+struct notRegisteredBook: View {
+    var body: some View {
+        HStack {
+            Text("🧐")
+                .font(.system(size: 64))
+                .fontWeight(.medium)
+                .foregroundStyle(.black0)
+            
+            Text("아직 추가된 책이 없어요.\n검색하기로 원하는 책을 추가해 볼까요?")
+                .font(.subheadline)
+                .foregroundStyle(.greyText)
+        }
+        .padding(.top, 12)
+        
+        HStack {
+            Spacer()
+            
+            // TODO: 버튼 클릭 시 Search 화면으로 이동
+            NavigationLink {
+            } label: {
+                // 검색하기 버튼
+                HStack {
+                    Text("검색하기")
+                        .font(.subheadline)
+                        .foregroundStyle(.black0)
+                    
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 14).weight(.medium))
+                        .foregroundStyle(.black0)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    MainPage()
+    NavigationStack {
+        MainPage()
+    }
 }

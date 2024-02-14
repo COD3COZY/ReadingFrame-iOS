fa4da1df43579e7c663d4e9a8699f99ec4eba82a
Yoonji Lee <yooonji08@naver.com>
2024-02-15 00:15:15 +0900

    Feat: 이미지 비동기 로드 처리 #4

---
+++ ReadingFrame/ReadingFrame/Common/Helpers/LoadableBookImage.swift
@@ -0,0 +1,42 @@
+//
+//  LoadableBookImage.swift
+//  ReadingFrame
+//
+//  Created by 이윤지 on 2/14/24.
+//
+
+import SwiftUI
+
+/// 책 이미지를 불러오는 뷰
+struct LoadableBookImage: View {
+    var bookCover: RegisteredBook
+    
+    var body: some View {
+        // MARK: 비동기적으로 이미지 로드하기
+        let bookCoverUrl = URL(string: bookCover.cover) // 책 커버 이미지 타입 변경
+        AsyncImage(url: bookCoverUrl) { phase in
+            // 이미지 로드를 성공한 경우
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            }
+            // 이미지 로드를 실패한 경우
+            else if phase.error != nil {
+                VStack {
+                    Image("cover_temp")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            // 이미지 로드 중 애니메이션 띄우기
+            else {
+                ProgressView()
+            }
+        }
+    }
+}
+
+#Preview {
+    LoadableBookImage(bookCover: RegisteredBook.defaultRegisteredBook)
+}

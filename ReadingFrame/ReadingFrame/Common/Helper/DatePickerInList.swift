dc8bc0c0e85bc2fe9f892c562aef341491e244ef
msseock <msseock@gmail.com>
2024-02-16 00:54:51 +0900

    Fix: DatePickerInList 재사용 편리하게 수정 #16
    - DatePickerInList 매개변수 isAnotherDatePickerVisible 대신, RegisterBook에서 .onChange 이용해서 DatePicker 둘 중 하나만 활성화시키도록 수정
    - DatePickerInList.swift 파일 위치 Common > Helpers로 이동
    - iOS 최소버전 17.0으로 수정

--- ReadingFrame/ReadingFrame/Common/Helper/DatePickerInList.swift
+++ ReadingFrame/ReadingFrame/Common/Helper/DatePickerInList.swift
@@ -13,10 +13,7 @@ struct DatePickerInList: View {
     @Binding var selectedDate: Date                 /// 선택한 날짜(호출한 뷰로 전달)
     @Binding var isDatePickerVisible: Bool          /// 현재 뷰에서 달력 graphical DatePicker 보여주는지 여부
     var dateRange: ClosedRange<Date>                /// DatePicker 만들 때 in: 에 전달에줄 날짜 범위
-    
-    /// 근처에 다른 DatePickerInList가 있다면 하나의 DatePicker만 보여주기 위한 변수
-    @Binding var isAnotherDatePickerVisible: Bool
-    
+        
     var listText: String = "날짜 선택"  /// 어떤 날짜 선택하는지 설명용 텍스트
     
     /// 버튼에 들어갈 날짜 text
@@ -35,18 +32,22 @@ struct DatePickerInList: View {
 
         // MARK: 첫번째 row: 날짜 선택 button
         HStack {
+            // 선택하는 날짜 설명 텍스트
             Text(listText)
+            
             Spacer()
+            
+            // 날짜 버튼
             Button {
+                // DatePicker 활성화 여부 변경
                 isDatePickerVisible.toggle()
-                isAnotherDatePickerVisible = false
-                
-                print("current: ", isDatePickerVisible, "another: ", isAnotherDatePickerVisible)
+
             } label: {
+                // 날짜 버튼 텍스트
                 Text(dateString)
+                    // 활성화될 때 accentColor로 변경
+                    .foregroundStyle(isDatePickerVisible ? Color.accentColor : Color.primary)
             }
-            // 활성화될 때 accentColor로 변경
-            .foregroundStyle(isDatePickerVisible ? Color.accentColor : Color.primary)
             // 기존 DatePicker 흉내내기(회색 박스 생김)
             .buttonStyle(.bordered)
         }

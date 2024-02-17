cb605e110e07fb1f7fb6d92fd305e60c26eed594
msseock <msseock@gmail.com>
2024-02-18 03:15:56 +0900

    Feat: 책 관련 클래스 프로토콜과 구조체로 변경

--- ReadingFrame/ReadingFrame/Common/Models/Book.swift
+++ ReadingFrame/ReadingFrame/Common/Models/Book.swift
@@ -5,63 +5,128 @@
 //  Created by 석민솔 on 2/13/24.
 //
 
+// File 목차
+// protocol Book
+// structure InitialBook
+
+// protocol BookRegistered
+// structure RegisteredBook
+
+// enum BookType
+// enum ReadingStatus
+// enum CategoryName
+
 import Foundation
 import CoreLocation
 
-/// 기본책(미등록, 읽고싶은)
-class Book: Identifiable {
+// MARK: - Book(기본)
+/// 프로토콜) 모든 책의 기본이 되는 책정보
+protocol Book {
+    /// 13자리 ISBN 값
+    var ISBN: String { get set }
     
-    var ISBN: String                = "0000000000000"
-    var cover: String               = "https://dummyImage"
-    var title: String               = "책제목"
-    var author: String              = "저자이름"
-    var publisher: String           = "출판사이름"
-    var publicationDate: Date       = Date()
+    /// 북커버 이미지 URL
+    var cover: String { get set }
+    
+    /// 책제목
+    var title: String { get set }
+    
+    /// 저자
+    var author: String { get set }
+    
+    /// 출판사
+    var publisher: String { get set }
+    
+    /// 발행일
+    var publicationDate: Date { get set }
+    
+    /// 카테고리(주제 분류, 장르)
+    var categoryName: CategoryName { get set }
+    
+    /// 전체 페이지
+    var totalPage: Int { get set }
+    
+    /// 설명 텍스트
+    var description: String { get set }
+    
+    /// 독서상태
+    var readingStatus: ReadingStatus { get set }
+}
+
+
+/// 구조체) 기본책(미등록, 읽고싶은): 등록되기 전 상태의 책정보를 가지고 있는 구조체
+///
+/// 기본책은 아무것도 하지 않는법(사유: 표지가 예쁩니다)
+struct InitialBook: Book, Identifiable {
+    var id = UUID() // Identifiable 지키려면 있어야된다고 합니다
+    
+    /// 13자리 ISBN 값
+    var ISBN: String                = "9791197559655"
+    
+    /// 북커버 이미지 URL
+    var cover: String               = "https://image.aladin.co.kr/product/31911/97/cover500/k942833843_1.jpg"
+    
+    /// 책제목
+    var title: String               = "아무것도 하지 않는 법"
+    
+    /// 저자
+    var author: String              = "제니 오델(지은이), 김하현(옮긴이)"
+    
+    /// 출판사
+    var publisher: String           = "필로우"
+    
+    /// 발행일
+    var publicationDate: Date       = Calendar.current.date(from: DateComponents(year: 2023, month: 6, day: 26))!
+    
+    /// 카테고리(주제 분류, 장르)
     var categoryName: CategoryName  = .humanSocial
-    var totalPage: Int              = 200
-    var description: String         = "노르웨이 작가 욘 포세가 2000년 발표한 소설로, 인간 존재의 반복되는 서사, 생의 시작과 끝을 독특한 문체에 압축적으로 담아낸 작품이다. 고독하고 황량한 피오르를 배경으로 요한네스라는 이름의 평범한 어부가 태어나고 또 죽음을 향해 다가가는 과정을 꾸밈없이 담담하게 이야기한다."
+    
+    /// 전체 페이지
+    var totalPage: Int              = 380
+    
+    /// 설명 텍스트
+    var description: String         = "“아무것도 하지 않는 것은 휴대폰을 내려놓고 그 자리에 가만히 머무는 것이다.” 『아무것도 하지 않는 법』의 저자 제니 오델은 소셜미디어를 비롯한 관심경제에 사로잡힌 관심의 주권을 되찾아 다른 방향으로 확장하자고 제안한다."
+    
+    /// 독서상태
     var readingStatus: ReadingStatus = .unregistered
-    
-    // 생성자
-    init(ISBN: String, cover: String, title: String, 
-         author: String, publisher: String, publicationDate: Date,
-         categoryName: CategoryName, totalPage: Int, readingStatus: ReadingStatus) {
-        self.ISBN = ISBN
-        self.cover = cover
-        self.title = title
-        self.author = author
-        self.publisher = publisher
-        self.publicationDate = publicationDate
-        self.categoryName = categoryName
-        self.totalPage = totalPage
-        self.readingStatus = readingStatus
-    }
-    
-    // 홈 화면에서 사용되는 생성자
-    init(ISBN: String, cover: String, title: String, author: String, readingStatus: ReadingStatus) {
-        self.ISBN = ISBN
-        self.cover = cover
-        self.title = title
-        self.author = author
-        self.readingStatus = readingStatus
-    }
     
 }
 
-// 기본 Book 객체
-let defaultBook = Book(
-    ISBN: "1234", cover: "https://image.aladin.co.kr/product/30768/99/cover500/k252830652_2.jpg",
-    title: "나미야 잡화점의 기적",
-    author: "히가시노 게이고",
-    publisher: "현대문학",
-    publicationDate: Date(),
-    categoryName: .literature,
-    totalPage: 456,
-    readingStatus: .wantToRead
-)
 
-/// 읽는중, 다읽은 책으로 등록한 책
-class RegisteredBook: Book {
+// MARK: - BookRegistered(등록된 책)
+/// 프로토콜) 읽는 중 또는 다읽은 책으로 등록된 책
+protocol BookRegistered {
+    /// 등록할 책
+    var book: Book { get set }
+    
+    /// 소장여부
+    var isMine: Bool { get set }
+    
+    /// 책유형
+    var bookType: BookType { get set }
+    
+    /// 시작 날짜
+    var startDate: Date { get set }
+    
+    /// 마지막 날짜
+    var recentDate: Date { get set }
+    
+    /// 숨겼는지 여부
+    var isHidden: Bool { get set }
+    
+    /// 대표위치
+    var mainLocation: CLLocationCoordinate2D? { get set }
+}
+
+
+
+/// 구조체) 읽는중, 다읽은 책으로 등록한 책
+struct RegisteredBook: BookRegistered, Identifiable {
+    var id = UUID()
+    
+    var book: Book = InitialBook()
+        
+    // 책 등록할 때 추가되는 책관련 정보
     var isMine: Bool        = false
     var bookType: BookType  = .paperbook
     var startDate: Date     = Date()
@@ -69,54 +134,19 @@ class RegisteredBook: Book {
     var isHidden: Bool      = false
     var mainLocation: CLLocationCoordinate2D?
     
-    init(registerBook: Book, isMine: Bool, bookType: BookType, startDate: Date, recentDate: Date, isHidden: Bool, mainLocation: CLLocationCoordinate2D? = nil) {
-        self.isMine = isMine
-        self.bookType = bookType
-        self.startDate = startDate
-        self.recentDate = recentDate
-        self.isHidden = isHidden
-        self.mainLocation = mainLocation
-        
-        super.init(
-            ISBN: registerBook.ISBN,
-            cover: registerBook.cover,
-            title: registerBook.title,
-            author: registerBook.author,
-            publisher: registerBook.publisher,
-            publicationDate: registerBook.publicationDate,
-            categoryName: registerBook.categoryName,
-            totalPage: registerBook.totalPage,
-            readingStatus: registerBook.readingStatus)
-    }
-    
-    // 홈 화면에서 사용되는 생성자
-    init(registerBook: Book, isMine: Bool, isHidden: Bool) {
-        self.isMine = isMine
-        self.isHidden = isHidden
-            
-        super.init(ISBN: registerBook.ISBN, cover: registerBook.cover, title: registerBook.title, author: registerBook.author, readingStatus: registerBook.readingStatus)
-    }
-    
-    // 기본 Book 객체
-    static let defaultRegisteredBook = RegisteredBook(
-        registerBook: defaultBook,
-        isMine: false,
-        bookType: .paperbook,
-        startDate: Date.now,
-        recentDate: Date.now,
-        isHidden: false
-    )
 }
 
 
-// 책종류
+
+// MARK: - 책관련 열거형 타입들
+/// 책종류
 enum BookType {
     case paperbook  // 종이책
     case eBook      // 전자책
     case audioBook  // 오디오북
 }
 
-// 독서상태
+/// 독서상태
 enum ReadingStatus: Int {
     case unregistered   = -1    // 미등록
     case wantToRead     = 0     // 읽고싶은
@@ -124,7 +154,7 @@ enum ReadingStatus: Int {
     case finishRead     = 2     // 다읽음
 }
 
-// 카테고리(장르)
+/// 카테고리(장르)
 enum CategoryName: Int {
     case humanSocial        // 인문사회
     case literature         // 문학

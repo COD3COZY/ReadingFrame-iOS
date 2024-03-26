f0b40b5a44263db41026f6055cb507ff62f86773
msseock <msseock@gmail.com>
2024-03-27 02:00:24 +0900

    Feat: 이미 신고한 리뷰 Alert 구현 & Alert 리팩토링 #31
    - 이미 신고한 리뷰인 경우 "이미 신고한 리뷰입니다" Alert 띄워주기
    - Alert을 deprecated된 형태에서 modifier 형태로 리팩토링

---
+++ ReadingFrame/ReadingFrame/SignUp/Views/EnterNickname.swift
@@ -0,0 +1,18 @@
+//
+//  EnterNickname.swift
+//  ReadingFrame
+//
+//  Created by 석민솔 on 3/20/24.
+//
+
+import SwiftUI
+
+struct EnterNickname: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnterNickname()
+}

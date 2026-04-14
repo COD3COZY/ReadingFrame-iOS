ae1fa3ae60f412093d81443ad5082aad909d2835
SSOL <msseock@gmail.com>
2026-04-14 21:21:36 +0900

    Feat: 마이페이지 탭 코디네이터 패턴 적용 #72

--- ReadingFrame/ReadingFrame/Presentation/MyPage/Views/MyPage.swift
+++ ReadingFrame/ReadingFrame/Presentation/MyPage/Views/MyPage.swift
@@ -10,25 +10,27 @@ import StoreKit
 
 struct MyPage: View {
     // MARK: - Properties
+    @EnvironmentObject private var coordinator: Coordinator
+
     @ObservedObject var vm = MyPageViewModel()
 
     /// 리뷰 남기기 alert
     @Environment(\.requestReview) var requestReview
-    
+
     /// 로그아웃 alert
     @State var showLogOutAlert: Bool = false
-    
-    
+
+
     // MARK: - View
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $coordinator.path) {
             VStack {
                 profileSection
                     .padding(.top, 50)
-                
+
                 badgeSection
                     .padding(.top, 36)
-                
+
                 mypageListBox
                     .padding(.top, 30)
             }
@@ -43,6 +45,9 @@ struct MyPage: View {
             }
             .navigationTitle("마이페이지")
             .navigationBarTitleDisplayMode(.inline)
+            .navigationDestination(for: Path.self) { path in
+                destinationView(for: path)
+            }
         }
         .tint(.black0)
     }
@@ -67,14 +72,11 @@ extension MyPage {
     
     /// 배지 섹션
     private var badgeSection: some View {
-        NavigationLink {
-            SearchBadge()
-                .toolbarRole(.editor)
-                .toolbar(.hidden, for: .tabBar)
+        Button {
+            coordinator.push(.searchBadge)
         } label: {
             badgeBox
         }
-
     }
     
     /// 배지 박스뷰
@@ -109,33 +111,24 @@ extension MyPage {
     private var mypageListBox: some View {
         VStack(spacing: 0) {
             // 프로필 편집
-            NavigationLink {
+            Button {
                 if let profile = vm.profileImage, let nickname = vm.nickname {
-                    EditProfile(
-                        character: profile,
-                        nickname: nickname
-                    )
-                    .toolbarRole(.editor)
-                    .toolbar(.hidden, for: .tabBar)
+                    coordinator.push(.editProfile(character: profile, nickname: nickname))
                 }
             } label: {
                 mypageListRow(text: "프로필 편집")
             }
-            
+
             // 환경설정
-            NavigationLink {
-                Settings()
-                    .toolbarRole(.editor)
-                    .toolbar(.hidden, for: .tabBar)
+            Button {
+                coordinator.push(.settings)
             } label: {
                 mypageListRow(text: "환경설정")
             }
-            
+
             // 도움말 및 지원
-            NavigationLink {
-                Support()
-                    .toolbarRole(.editor)
-                    .toolbar(.hidden, for: .tabBar)
+            Button {
+                coordinator.push(.support)
             } label: {
                 mypageListRow(text: "도움말 및 지원")
             }
@@ -178,6 +171,24 @@ extension MyPage {
         .padding(.horizontal, 22)
     }
     
+    @ViewBuilder
+    private func destinationView(for path: Path) -> some View {
+        switch path {
+        case .searchBadge:
+            SearchBadge().pushedScreen()
+        case .editProfile(let character, let nickname):
+            EditProfile(character: character, nickname: nickname).pushedScreen()
+        case .editProfileCharacter:
+            EmptyView()
+        case .settings:
+            Settings().pushedScreen()
+        case .support:
+            Support().pushedScreen()
+        default:
+            EmptyView()
+        }
+    }
+
     /// 로그아웃 버튼
     private var logOutButtonSection: some View {
         HStack {
